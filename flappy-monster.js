4e25aee339156b3fc8de7b3c7bcff412e8c7c91d
aalopez12121 <72616521+aalopez12121@users.noreply.github.com>
2021-01-22 20:06:02 -0500

    Update flappy-monster.js

--- flappy-monster.js
+++ flappy-monster.js
@@ -2,6 +2,10 @@ var INITIAL = 1;
 var GAME_PLAYING = 2;
 var GAME_OVER = 3;
 
+var KEY_CODE = {
+  R: 82
+};
+
 function FlappyMonster(canvas) {
 // Base
   var game = this;
@@ -12,13 +16,21 @@ function FlappyMonster(canvas) {
 
   // Game State
   game.currentState = INITIAL;
+ 
+  // Create Game Objects
+  game.createObjects();
+
 
   // Start Game
   window.requestAnimationFrame(function() {
     game.runGameLoop();
   });
 
-INITIAL--
+FlappyMonster.prototype.createObjects = function() {
+  // Base
+  var game = this;
+
+  INITIAL--
  // Game State
   game.currentState = INITIAL;
 
@@ -49,18 +61,71 @@ FlappyMonster.prototype.start = function() {
   // Base
   var game = this;
 
+        window.requestAnimationFrame(function() {
+    game.runGameLoop();
+  });
+
+
  // DRAW INITIAL SCREEN
       game.drawInitialScreen();
       break;
-    case GAME_OVER:
-      // DRAW GAME OVER SCREEN
-          break;
+  
       case GAME_OVER:
       // DRAW GAME OVER SCREEN
       game.drawGameOverScreen();
       break;
+// Draw
+ game.context.fillText('Click to Start!', game.canvas.width / 2 - 100, game.canvas.height / 2);
+
+  // Background
+  game.context.fillStyle = 'black';
+  game.context.fillRect(0, 0, game.canvas.width, game.canvas.height);
+
+  // Text
+  game.context.fillStyle = 'white';
+  game.context.font = '36px Arial';
+  game.context.fillText('INITIAL', game.canvas.width / 2 - 100, game.canvas.height / 2);
+  // Bind Events
+  game.bindEvents();
+Right under states
+
+FlappyMonster.prototype.bindEvents = function() {
+  // Base
+  var game = this;
+
+  // Mouse Listener
+  game.canvas.addEventListener('click', function(event) {
+    switch (game.currentState) {
+      case INITIAL:
+        game.currentState = GAME_PLAYING;
+        break;
+      case GAME_PLAYING:
+        // DRAW GAME PLAYING SCREEN
+        break;
+    }
+  });
+
+// Key Listener
+  window.addEventListener('keydown', function(event) {
+    switch (game.currentState) {
+      case GAME_OVER:
+        if(event.keyCode === KEY_CODE.R){
+          console.log(event.keyCode);
+          game.currentState = GAME_PLAYING;
+        }
+        break;
+    }
+  });
+};
+  
 
   }
+//text
+game.context.fillStyle = 'white';
+  game.context.font = '36px Arial';
+  game.context.fillText('Game Over :(', game.canvas.width / 2 - 100, game.canvas.height / 2);
+  game.context.font = '24px Arial';
+  game.context.fillText('Press R to Restart!', game.canvas.width / 2 - 100, game.canvas.height / 2 + 50);
 
 
 
