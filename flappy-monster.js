24feb947ada9915dc617e1cbe6dfc7c52c97cefd
aalopez12121 <72616521+aalopez12121@users.noreply.github.com>
2021-01-22 19:53:39 -0500

    Update flappy-monster.js

--- flappy-monster.js
+++ flappy-monster.js
@@ -1,3 +1,7 @@
+var INITIAL = 1;
+var GAME_PLAYING = 2;
+var GAME_OVER = 3;
+
 function FlappyMonster(canvas) {
 // Base
   var game = this;
@@ -9,5 +13,54 @@ function FlappyMonster(canvas) {
   // Game State
   game.currentState = INITIAL;
 
+  // Start Game
+  window.requestAnimationFrame(function() {
+    game.runGameLoop();
+  });
+
+INITIAL--
+ // Game State
+  game.currentState = INITIAL;
 
 }
+FlappyMonster.prototype.start = function() {
+   // Base
+  var game = this;
+}
+
+FlappyMonster.prototype.start = function() {
+   // Base
+  var game = this;
+}
+// Game State
+  switch (game.currentState) {
+    case INITIAL:
+      // DRAW INITIAL SCREEN
+          break;
+   INITIAL
+// Game State
+  switch (game.currentState) {
+    case INITIAL:
+      FlappyMonster.prototype.drawInitialScreen = function() {
+  // Base
+  var game = this;
+}
+      FlappyMonster.prototype.drawGameOverScreen = function() {
+  // Base
+  var game = this;
+
+ // DRAW INITIAL SCREEN
+      game.drawInitialScreen();
+      break;
+    case GAME_OVER:
+      // DRAW GAME OVER SCREEN
+          break;
+      case GAME_OVER:
+      // DRAW GAME OVER SCREEN
+      game.drawGameOverScreen();
+      break;
+
+  }
+
+
+
