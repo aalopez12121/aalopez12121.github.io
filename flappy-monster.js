3bef673f4c98df7e0aa1a2d2cf84bb670e96a3c0
aalopez12121 <72616521+aalopez12121@users.noreply.github.com>
2021-01-29 11:19:05 -0500

    Update flappy-monster.js

--- flappy-monster.js
+++ flappy-monster.js
@@ -2,6 +2,7 @@ var INITIAL = 1;
 var GAME_PLAYING = 2;
 var GAME_OVER = 3;
 
+
 var KEY_CODE = {
   R: 82
 };
@@ -16,62 +17,43 @@ function FlappyMonster(canvas) {
 
   // Game State
   game.currentState = INITIAL;
-
+  
   // Game Speed
   game.velocity = 5;
 
-  // Bind Events
-  game.bindEvents();
 
-  // Create Game Objects
+    // Bind Events
+  game.bindEvents();
+  
+   // Create Game Objects
   game.createObjects();
+  
+  
 }
 
 FlappyMonster.prototype.createObjects = function() {
   // Base
   var game = this;
 
-  Monster.prototype.draw = function() {
-  // Base
-  var monster = this;
-
-  // Draw
-  if(monster.img != null){
-     monster.vy = monster.vy + monster.g;
-     monster.y = monster.y + monster.vy;
-
-     if(monster.y + monster.h > monster.canvas.height){
-       monster.y = monster.canvas.height - monster.h;
-       monster.vy = 0;
-     }else if(monster.y < 0){
-       monster.y = 0;
-       monster.vy = 0;
-     }
-
-    monster.context.drawImage(monster.img, monster.frame * 115, 0, 115, 100, monster.x, monster.y, monster.w, monster.h);
-    monster.frame++;
-    monster.frame %= 4;
-  }
-};
-
-  // Background
   game.background1 = new GameBackground('back.png', game.canvas);
   game.background2 = new GameBackground('back.png', game.canvas);
   game.background2.x = game.canvas.width;
 
-  // Score
+    // Score
   game.gameScore = new GameScore(game.canvas);
   game.gameScore.x = game.canvas.width - 150;
   game.gameScore.y = 80;
-
+  
   // Wall Factory
   game.wallFactory = new WallFactory(game.canvas);
-  game.wallFactory.generateWalls();
 
+  
   // Monster
   game.monster = new Monster('monster.png', game.canvas);
-
+  
 };
+
+
 
 FlappyMonster.prototype.bindEvents = function() {
   // Base
@@ -80,16 +62,31 @@ FlappyMonster.prototype.bindEvents = function() {
   // Mouse Listener
   game.canvas.addEventListener('click', function(event) {
     switch (game.currentState) {
-     game.wallFactory.generateWalls();
-     case INITIAL:
+      case INITIAL:
         game.currentState = GAME_PLAYING;
+        game.wallFactory.generateWalls();
         break;
-      game.reset();
-case GAME_PLAYING:
+      case GAME_PLAYING:
+        game.monster.vy = -1 * game.velocity;
         // DRAW GAME PLAYING SCREEN
         break;
     }
   });
+
+  // Key Listener
+  window.addEventListener('keydown', function(event) {
+    switch (game.currentState) {
+      case GAME_OVER:
+        if(event.keyCode === KEY_CODE.R){
+          console.log(event.keyCode);
+            game.reset();
+          game.currentState = GAME_PLAYING;
+        }
+        break;
+    }
+  });
+};
+
 FlappyMonster.prototype.reset = function() {
   // Base
   var game = this;
@@ -102,23 +99,13 @@ FlappyMonster.prototype.reset = function() {
   game.monster.y = 115;
 };
 
-  // Key Listener
-  window.addEventListener('keydown', function(event) {
-    switch (game.currentState) {
-      case GAME_OVER:
-        if(event.keyCode === KEY_CODE.R){
-          console.log(event.keyCode);
-          game.currentState = GAME_PLAYING;
-        }
-        break;
-    }
-  });
-};
+
+
 
 FlappyMonster.prototype.start = function() {
-  // Base
+   // Base
   var game = this;
-
+  
   // Start Game
   window.requestAnimationFrame(function() {
     game.runGameLoop();
@@ -126,11 +113,11 @@ FlappyMonster.prototype.start = function() {
 };
 
 FlappyMonster.prototype.runGameLoop = function() {
+  
   // Base
   var game = this;
-
-  // Game State
-  switch (game.currentState) {
+  
+ switch (game.currentState) {
     case INITIAL:
       // DRAW INITIAL SCREEN
       game.drawInitialScreen();
@@ -144,16 +131,14 @@ FlappyMonster.prototype.runGameLoop = function() {
       game.drawGameOverScreen();
       break;
   }
-
-  window.requestAnimationFrame(function() {
+   window.requestAnimationFrame(function() {
     game.runGameLoop();
   });
-};
 
+};
 FlappyMonster.prototype.drawInitialScreen = function() {
   // Base
   var game = this;
-
   // Draw
 
   // Background
@@ -163,10 +148,8 @@ FlappyMonster.prototype.drawInitialScreen = function() {
   // Text
   game.context.fillStyle = 'white';
   game.context.font = '36px Arial';
-  game.context.fillText('Click to Start!', game.canvas.width / 2 - 100, game.canvas.height / 2);
-  
-};
-
+  game.context.fillText('Click to Start', game.canvas.width / 2 - 100, game.canvas.height / 2);
+}
 FlappyMonster.prototype.drawGamePlayingScreen = function() {
   // Base
   var game = this;
@@ -177,27 +160,27 @@ FlappyMonster.prototype.drawGamePlayingScreen = function() {
   // Draw Background
   game.animateBackground();
 
-  // Draw Score
+// Draw Score
   game.gameScore.draw();
-
-  // Draw Walls
+  
+   // Draw Walls
   game.drawWalls();
 
-  console.log(game.wallFactory.walls);
+ //console.log(game.wallFactory.walls);
+// Draw Monster
+  game.monster.draw();
 
   // Collision Check
   game.checkCollisions();
-
 };
 
 FlappyMonster.prototype.checkCollisions = function() {
   // Base
   var game = this;
 var walls = game.wallFactory.walls;
-(game.isCollided(game.monster, walls[i])){
-      game.currentState = GAME_OVER;
-for(var i = 0; i < walls.length; i++){
-    if(){
+
+  for(var i = 0; i < walls.length; i++){
+    if(game.isCollided(game.monster, walls[i])){
       game.currentState = GAME_OVER;
     }
   }
@@ -234,8 +217,6 @@ FlappyMonster.prototype.drawWalls = function() {
 
   // Draw Walls
   var walls = game.wallFactory.walls;
-// Draw Monster
-  game.monster.draw();
 
   for(var i = 0; i < walls.length; i++){
     walls[i].draw();
@@ -243,8 +224,8 @@ FlappyMonster.prototype.drawWalls = function() {
   }
 
   game.removeExtraWalls();
-
 };
+
 
 FlappyMonster.prototype.removeExtraWalls = function() {
   // Base
@@ -286,7 +267,6 @@ FlappyMonster.prototype.animateBackground = function() {
 FlappyMonster.prototype.drawGameOverScreen = function() {
   // Base
   var game = this;
-
   // Draw
 
   // Background
@@ -300,57 +280,3 @@ FlappyMonster.prototype.drawGameOverScreen = function() {
   game.context.font = '24px Arial';
   game.context.fillText('Press R to Restart!', game.canvas.width / 2 - 100, game.canvas.height / 2 + 50);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
